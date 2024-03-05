090c4d0f517fe4fcba344560bc1f09f5c99f9c7a
vaibhav <vaibhavsonkusare2@gmail.com>
2024-03-06 00:00:47 +0530

    second commit

--- app/src/main/java/com/example/railquest/MainActivity.java
+++ app/src/main/java/com/example/railquest/MainActivity.java
@@ -11,41 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText userIdEditText;
-    private EditText passwordEditText;
-    private CheckedTextView checkedTextView;
-    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        userIdEditText = findViewById(R.id.editTextText);
-        passwordEditText = findViewById(R.id.editTextTextPassword);
-        checkedTextView = findViewById(R.id.checkedTextView);
-        submitButton = findViewById(R.id.button);
-    }
-
-    protected void onClickSubmit(View view) {
-        String userID = userIdEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
-        boolean isChecked = checkedTextView.isChecked();
-
-        if (userID.length() == 0) {
-            Toast.makeText(this, "Enter User ID", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (password.length() == 0) {
-            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (!isChecked) {
-            Toast.makeText(this, "Agree to all Terms and Conditions.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // start to login.
-
     }
 }
